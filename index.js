8dd5e43372dc5a6888a7050a4c1e01dbbf76b366
Addison's Home PC <addison.yarborough@weplaydots.com>
2021-08-18 11:15:30 -0400

    Setup for use with remote server

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 // Import express and create an app instance
 const app = require('express')();
 
+// Import cors module
+const cors = require('cors');
+
 // Import and mount the routes by passing them into the app
 app.use(require('./routes/serverTest'));
 app.use(require('./routes/start'));
 app.use(require('./routes/select'));
 
+// Use cors instance module in the app
+app.use(cors());
+
 // Define the server port
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Listener
-app.listen(PORT, () => console.log(`Server running at: http://localhost:${PORT}/`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
